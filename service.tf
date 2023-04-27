6947ae9279210e7235b433addbb32ebd2c7c6baf
sameeryalla <sameeryalla@gmail.com>
2023-04-27 07:52:46 +0530

    by using count loop automated instances creation and commented all individual blocks for instance creation

--- service.tf
+++ service.tf
@@ -1,3 +1,6 @@
+variable "components" {
+  default= [ "frontend","mongodb","catalogue","redis","user","cart","mysql","shipping","rabbitmq","payment","dispatch" ]
+}
 
 variable "instance_type" {
   default="t3.micro"
@@ -14,86 +17,96 @@ data "aws_security_group" "allow-all"{
 }
 
 
-
-resource "aws_instance" "frontend" {
+resource "aws_instance" "instance" {
+  count=length(var.components)
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
-    Name = "frontend"
+    Name = var.components[ count.index ]
   }
 }
-
-output "frontend" {
-  value=aws_instance.frontend.public_ip
-}
-
-resource "aws_route53_record" "frontend"{
-  zone_id = "Z04900482TS501XM50DYJ"
-  name="frontend.sameerdevops.online"
-  type="A"
-  ttl=30
-  records=[aws_instance.frontend.private_ip]
-}
-
-resource "aws_instance" "mongodb" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "mongodb"
-  }
-}
-
-output "mongodb" {
-  value=aws_instance.mongodb.public_ip
-}
-
-resource "aws_route53_record" "mongodb"{
-  zone_id = "Z04900482TS501XM50DYJ"
-  name="mongodb.sameerdevops.online"
-  type="A"
-  ttl=30
-  records=[aws_instance.mongodb.private_ip]
-}
-
-resource "aws_instance" "catalogue" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "Catalogue"
-  }
-}
-output "catalogue" {
-  value=aws_instance.catalogue.public_ip
-}
-
-resource "aws_instance" "redis" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "Redis"
-  }
-}
-output "redis" {
-  value=aws_instance.redis.public_ip
-}
-
-resource "aws_instance" "user" {
-  ami           = data.aws_ami.centos.image_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = "User"
-  }
-}
-output "user" {
-  value=aws_instance.user.public_ip
-}
+#by using above loop we automated below all instances creation so commented below code
+#resource "aws_instance" "frontend" {
+#  ami           = data.aws_ami.centos.image_id
+#  instance_type = var.instance_type
+#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+#
+#  tags = {
+#    Name = "frontend"
+#  }
+#}
+#
+#output "frontend" {
+#  value=aws_instance.frontend.public_ip
+#}
+#
+#resource "aws_route53_record" "frontend"{
+#  zone_id = "Z04900482TS501XM50DYJ"
+#  name="frontend.sameerdevops.online"
+#  type="A"
+#  ttl=30
+#  records=[aws_instance.frontend.private_ip]
+#}
+#
+#resource "aws_instance" "mongodb" {
+#  ami           = data.aws_ami.centos.image_id
+#  instance_type = var.instance_type
+#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+#
+#  tags = {
+#    Name = "mongodb"
+#  }
+#}
+#
+#output "mongodb" {
+#  value=aws_instance.mongodb.public_ip
+#}
+#
+#resource "aws_route53_record" "mongodb"{
+#  zone_id = "Z04900482TS501XM50DYJ"
+#  name="mongodb.sameerdevops.online"
+#  type="A"
+#  ttl=30
+#  records=[aws_instance.mongodb.private_ip]
+#}
+#
+#resource "aws_instance" "catalogue" {
+#  ami           = data.aws_ami.centos.image_id
+#  instance_type = var.instance_type
+#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+#
+#  tags = {
+#    Name = "Catalogue"
+#  }
+#}
+#output "catalogue" {
+#  value=aws_instance.catalogue.public_ip
+#}
+#
+#resource "aws_instance" "redis" {
+#  ami           = data.aws_ami.centos.image_id
+#  instance_type = var.instance_type
+#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+#
+#  tags = {
+#    Name = "Redis"
+#  }
+#}
+#output "redis" {
+#  value=aws_instance.redis.public_ip
+#}
+#
+#resource "aws_instance" "user" {
+#  ami           = data.aws_ami.centos.image_id
+#  instance_type = var.instance_type
+#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
+#
+#  tags = {
+#    Name = "User"
+#  }
+#}
+#output "user" {
+#  value=aws_instance.user.public_ip
+#}
