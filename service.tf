93d3fe92f52c987b5d68327c88865b0ce0c3f833
sameeryalla <sameeryalla@gmail.com>
2023-05-01 22:16:16 +0530

    roboshop image id code changes

--- service.tf
+++ service.tf
@@ -7,7 +7,7 @@ variable "instance_type" {
 }
 
 data "aws_ami" "centos" {
-  owner       = ["973714476881"]
+  owners       = ["973714476881"]
   most_recent = true
   name_regex = "Centos-8-DevOps-Practice"
 }
