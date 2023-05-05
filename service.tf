40e59f7d23780d4e144bb362cc59e5a3d9c2cc1a
sameeryalla <sameeryalla@gmail.com>
2023-05-05 23:04:01 +0530

    parameterized provisioner code to run with module passwords modified 2

--- service.tf
+++ service.tf
@@ -22,14 +22,16 @@ resource "aws_route53_record" "records"{
 
 
 resource "null_resource" "provisioner" {
+
   depends_on = [aws_instance.instance,aws_route53_record.records]
   for_each = var.components
   provisioner "remote-exec" {
+
     connection {
       type = "ssh"
-      user= "centos"
-      password= "DevOps321"
-      host=aws_instance.instance[each.value["name"]].public_ip
+      user = "centos"
+      password = "DevOps321"
+      host = aws_instance.instance[each.value["name"]].private_ip
     }
     inline=[
       "rm -rf learn-shell",
