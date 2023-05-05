bde4c27bb21747f9da6f7dad888a454fe3299a09
sameeryalla <sameeryalla@gmail.com>
2023-05-05 20:23:41 +0530

    code clean up done for state file

--- state.tf
+++ state.tf
@@ -1,3 +1,7 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket = "roboshop-terraform2"
+    key    = "roboshop/dev/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
--- vars.tf
+++ vars.tf
@@ -1,6 +1,53 @@
 variable "env"{}
 
-variable "components" {}
+variable "components" {
+  default={
+    frontend={
+      name="frontend"
+      instance_type="t3.small"
+    }
+    mongodb={
+      name="mongodb"
+      instance_type="t3.small"
+    }
+    catalogue={
+      name="catalogue"
+      instance_type="t3.micro"
+    }
+    redis={
+      name="redis"
+      instance_type="t3.small"
+    }
+    user={
+      name="user"
+      instance_type="t3.micro"
+    }
+    cart={
+      name="cart"
+      instance_type="t3.micro"
+    }
+    mysql={
+      name="mysql"
+      instance_type="t3.small"
+    }
+    shipping={
+      name="shipping"
+      instance_type="t3.medium"
+    }
+    rabbitMQ={
+      name="rabbitMQ"
+      instance_type="t3.small"
+    }
+    payment={
+      name="payment"
+      instance_type="t3.small"
+    }
+    dispatch={
+      name="dispatch"
+      instance_type="t3.small"
+    }
+  }
+}
 
 variable "components2" {
   default= [ "frontend","mongodb","catalogue","redis","user","cart","mysql","shipping","rabbitmq","payment","dispatch" ]
