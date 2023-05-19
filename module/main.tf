4feb72a68a03bd6ac827a36934145e14891e90e1
sameeryalla <sameeryalla@gmail.com>
2023-05-19 20:48:41 +0530

    created policy and role resources to assign to instances

--- module/main.tf
+++ module/main.tf
@@ -30,3 +30,55 @@ resource "aws_route53_record" "records"{
   ttl=30
   records=[aws_instance.instance.private_ip]
 }
+
+resource "aws_iam_role" "role"{
+  name = "${var.component_name}-${var.env}-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${var.component_name}-${var.env}-role"
+  }
+}
+
+resource "aws_iam_policy" "ssm-ps-policy" {
+  name        = "${var.component_name}-${var.env}-ssm-ps-policy"
+  role        = aws_iam_role.role.id
+  description = "My test policy"
+
+  policy = jsonencode(
+    {
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Sid": "VisualEditor0",
+          "Effect": "Allow",
+          "Action": [
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource": "arn:aws:ssm:us-east-1:245803268481:parameter/${var.env}.${var.component_name}.*"
+        },
+        {
+          "Sid": "VisualEditor1",
+          "Effect": "Allow",
+          "Action": "ssm:DescribeParameters",
+          "Resource": "*"
+        }
+      ]
+    }
+  )
+}
