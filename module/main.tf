03576f5896c4a23d755dd0c9f30bb4b16b081294
sameeryalla <sameeryalla@gmail.com>
2023-05-23 20:19:32 +0530

    terraform code change 2

--- module/main.tf
+++ module/main.tf
@@ -59,7 +59,6 @@ resource "aws_iam_instance_profile" "instance_profile"{
 resource "aws_iam_policy" "ssm-ps-policy" {
   name        = "${var.component_name}-${var.env}-ssm-ps-policy"
   role        = aws_iam_role.role.id
-  description = "My test policy"
 
   policy = jsonencode(
     {
