f5aa411de0156b937ddd8d6168ce6a79c2364b83
sameeryalla <sameeryalla@gmail.com>
2023-05-23 20:08:21 +0530

    terraform code change

--- module/main.tf
+++ module/main.tf
@@ -52,8 +52,8 @@ resource "aws_iam_role" "role"{
 }
 
 resource "aws_iam_instance_profile" "instance_profile"{
-  name: "${var.component_name}-${var.env}-role"
-  role: aws_iam_role.role.name
+  name= "${var.component_name}-${var.env}-role"
+  role= aws_iam_role.role.name
 }
 
 resource "aws_iam_policy" "ssm-ps-policy" {
