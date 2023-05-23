ca251d075bf373f6c92625c1284728e37deaffd1
sameeryalla <sameeryalla@gmail.com>
2023-05-23 21:36:58 +0530

    raghu code deployed 4

--- module/locals.tf
+++ module/locals.tf
@@ -2,13 +2,13 @@ locals {
   name = var.env != "" ? "${var.component_name}-${var.env}" : var.component_name
   db_commands = [
     "rm -rf roboshop-shell",
-    "git clone https://github.com/raghudevopsb72/roboshop-shell",
+    "git clone https://github.com/sameeryalla/learn-shell",
     "cd roboshop-shell",
     "sudo bash ${var.component_name}.sh ${var.password}"
   ]
   app_commands = [
     "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/raghudevopsb72/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+    "ansible-pull -i localhost, -U https://github.com/sameeryalla/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
   db_tags = {
     Name = "${var.component_name}-${var.env}"
