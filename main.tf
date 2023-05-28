daf3ad7ded10c19cd5b036e0af4aff8c714e8ffa
sameeryalla <sameeryalla@gmail.com>
2023-05-28 23:37:39 +0530

    vpc creation tags added 2

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ module "vpc" {
        source = "git::https://github.com/sameeryalla/tf-module-vpc.git"
        for_each=var.vpc
        cidr_block=each.value["cidr_block"]
-       tags=var.tags
+       tags = local.tags
 }
