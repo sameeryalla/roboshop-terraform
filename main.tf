f24e51cb66375357397a767a14c2d623916f7099
sameeryalla <sameeryalla@gmail.com>
2023-06-01 00:17:50 +0530

    removed unnecessary variables 3

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "vpc" {
        for_each = var.vpc
        cidr_block = each.value["cidr_block"]
        subnets = each.value["subnets"]
-       azs = each.value["azs"]
        tags = local.tags
        env = var.env
 }
